4ea829641b5ed8eec39c9741e683025c0b0479a4
SLarkZ01 <tmontoyamagon11@gmail.com>
2025-04-01 15:32:36 -0500

    subida del modelo

---
+++ app/Models/Producto.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+    // Nombre de la tabla
+    protected $table = 'productos';
+    // Nombre de la llave primaria
+    protected $primaryKey = 'id';
+    // Define qué columnas pueden ser asignadas masivamente
+    protected $fillable = ['nombre', 'precio', 'stock'];
+}

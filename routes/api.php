aab235e1761d810f729d0798b58601b9e966403d
SLarkZ01 <tmontoyamagon11@gmail.com>
2025-05-05 18:47:50 -0500

    cambios en api.php comentado la ruta de productos

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('productos', App\Http\Controllers\ApiProductosController::class);
+/* Route::apiResource('productos', App\Http\Controllers\ApiProductosController::class); */
 
 Route::apiResource('categorias', App\Http\Controllers\ApiCategoriaController::class);
